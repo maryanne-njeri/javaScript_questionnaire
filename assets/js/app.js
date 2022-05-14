0e0e6e8eff7d99e30eeb45e0eef3d78e005c6b57
maryanne-rose <rosemary.njeri@student.moringaschool.com>
2022-05-14 19:25:29 +0300

    displays questions in the html markup from js file

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,11 @@
+// Select dom Element
 let continue_btn = document.querySelector('.continue-btn');
 let intro = document.querySelector('.intro');
 let board = document.querySelector('.quiz-board');
 
+// Hides the intro page content and displays Quiz Board
 function showQuizBoard() {
+    // Modify dom Element
     if (intro.style.display === 'none') {
         intro.style.display = 'block';
         board.style.display = 'none';
@@ -12,4 +15,26 @@ function showQuizBoard() {
     }
 }
 
-continue_btn.addEventListener('click', showQuizBoard);
+// Questions Object
+let questions = {
+    titles: [
+        'Why Javascript ?', 
+        'What are JavaScript Data Types ?', 
+        'What is the use of isNaN function ?',
+        'Which company developed JavaScript ?',
+        'What is ‘this’ keyword in JavaScript ?',
+    ],
+}
+
+// Iterate through each quiz and display them all
+function showQuestions(quiz) {
+
+    // selecting by a query
+    let titleDivs = document.querySelectorAll('#title');
+
+    titleDivs.forEach(function(question, key) {
+        question.textContent = quiz.titles[key];
+    });
+}
+
+showQuestions(questions);
