2adec94c93d80b58e0d0a0eabface14baa5aee58
maryanne-rose <rosemary.njeri@student.moringaschool.com>
2022-05-15 20:47:42 +0300

    refactors css file

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,40 +1,183 @@
-// Select dom Element
-let continue_btn = document.querySelector('.continue-btn');
-let intro = document.querySelector('.intro');
-let board = document.querySelector('.quiz-board');
-
-// Hides the intro page content and displays Quiz Board
-function showQuizBoard() {
-    // Modify dom Element
-    if (intro.style.display === 'none') {
-        intro.style.display = 'block';
-        board.style.display = 'none';
-    } else {
-        intro.style.display = 'none';
-        board.style.display = 'block';
+(function() {
+    
+    // Functions
+    function showQuizBoard() {
+        if (intro.style.display === 'none') {
+            intro.style.display = 'block';
+            board.style.display = 'none';
+        } else {
+            intro.style.display = 'none';
+            board.style.display = 'block';
+        }
     }
-}
 
-// Questions Object
-let questions = {
-    titles: [
-        'Why Javascript ?', 
-        'What are JavaScript Data Types ?', 
-        'What is the use of isNaN function ?',
-        'Which company developed JavaScript ?',
-        'What is ‘this’ keyword in JavaScript ?',
-    ],
-}
+    function buildQuiz() {
+        // variable to store the HTML output
+        const output = [];
 
-// Iterate through each quiz and display them all
-function showQuestions(quiz) {
+        // for each question...
+        myQuestions.forEach(
+        (currentQuestion, questionNumber) => {
 
-    // selecting by a query
-    let titleDivs = document.querySelectorAll('#title');
+            // variable to store the list of possible answers
+            const answers = [];
 
-    titleDivs.forEach(function(question, key) {
-        question.textContent = quiz.titles[key];
-    });
-}
+            // and for each available answer...
+            for(letter in currentQuestion.answers){
 
-showQuestions(questions);
+                // ...add an HTML radio button
+                answers.push(
+                    `<label>
+                    <input type="radio" name="question${questionNumber}" value="${letter}">
+                    ${letter} :
+                    ${currentQuestion.answers[letter]}
+                    </label>`
+                );
+            }
+
+            // add this question and its answers to the output
+            output.push(
+            `<div class="slide">
+                <div class="question"> ${currentQuestion.question} </div>
+                <div class="answers"> ${answers.join("")} </div>
+            </div>`
+            );
+        });
+
+        // finally combine our output list into one string of HTML and put it on the page
+        quizContainer.innerHTML = output.join('');
+    }
+
+    function showResults() {
+
+        // gather answer containers from our quiz
+        const answerContainers = quizContainer.querySelectorAll('.answers');
+
+        // keep track of user's answers
+        let numCorrect = 0;
+
+        // for each question...
+        myQuestions.forEach( (currentQuestion, questionNumber) => {
+
+            // find selected answer
+            const answerContainer = answerContainers[questionNumber];
+            const selector = `input[name=question${questionNumber}]:checked`;
+            const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+
+            // if answer is correct
+            if(userAnswer === currentQuestion.correctAnswer) {
+
+                // add to the number of correct answers
+                numCorrect++;
+
+                // color the answers green
+                answerContainers[questionNumber].style.color = 'lightgreen';
+            }
+
+            // if answer is wrong or blank
+            else {
+                // color the answers red
+                answerContainers[questionNumber].style.color = 'red';
+            }
+        });
+
+        // show number of correct answers out of total
+        resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
+    }
+
+    function showSlide(n) {
+
+        slides[currentSlide].classList.remove('active-slide');
+
+        slides[n].classList.add('active-slide');
+
+        currentSlide = n;
+
+        if(currentSlide === 0){
+            previousButton.style.display = 'none';
+        }
+
+        else {
+            previousButton.style.display = 'inline-block';
+        }
+
+        if(currentSlide === slides.length-1) {
+            nextButton.style.display = 'none';
+            submitButton.style.display = 'inline-block';
+        }
+
+        else {
+            nextButton.style.display = 'inline-block';
+            submitButton.style.display = 'none';
+        }
+    }
+
+    function showNextSlide() {
+        showSlide(currentSlide + 1);
+    }
+
+    function showPreviousSlide() {
+        showSlide(currentSlide - 1);
+    }
+
+    // Variables
+    const continue_btn = document.querySelector('.continue-btn');
+    const intro = document.querySelector('.intro');
+    const board = document.querySelector('.quiz-board');
+    const quizContainer = document.getElementById('quiz');
+    const resultsContainer = document.getElementById('results');
+    const submitButton = document.getElementById('submit');
+    const myQuestions = [
+        {
+            question: "Who invented JavaScript?",
+            answers: {
+                a: "Douglas Crockford",
+                b: "Sheryl Sandberg",
+                c: "Brendan Eich"
+            },
+        
+            correctAnswer: "c"
+
+        },
+        {
+            question: "Which one of these is a JavaScript package manager?",
+            answers: {
+                a: "Node.js",
+                b: "TypeScript",
+                c: "npm"
+            },
+
+            correctAnswer: "c"
+        },
+        {
+            question: "Which tool can you use to ensure code quality?",
+            answers: {
+                a: "Angular",
+                b: "jQuery",
+                c: "RequireJS",
+                d: "ESLint"
+            },
+
+            correctAnswer: "d"
+        }
+    ];
+
+    
+    // Kick things off
+    buildQuiz();
+    
+    // Pagination
+    const previousButton = document.getElementById("previous");
+    const nextButton = document.getElementById("next");
+    const slides = document.querySelectorAll(".slide");
+    let currentSlide = 0;
+    
+    // Show the first slide
+    showSlide(currentSlide);
+    
+    // Event listeners
+    continue_btn.addEventListener('click', showQuizBoard);
+    submitButton.addEventListener('click', showResults);
+    previousButton.addEventListener("click", showPreviousSlide);
+    nextButton.addEventListener("click", showNextSlide);
+})();
